7d5ccda360679b6cfc04af40bf0069d12e782801
Guozx <jiujiuli@qq.com>
2019-09-02 17:07:01 +0800

    init commit

---
+++ csv2xml.py
@@ -0,0 +1,16 @@
+import pandas
+import sys
+
+KEY_START = '<string name="{}">'
+KEY_END = '</string>'
+result = ""
+print(sys.getdefaultencoding())
+csv = pandas.read_csv("strings.csv")
+for index, row in csv.iterrows():
+    start = KEY_START.format(row[0])
+    result = result + start + row[2] + KEY_END + '\n'
+result.encode("GBK", 'ignore')
+print(result)
+with open('strings.xml', 'w', encoding='utf-8') as f:
+    f.write(result)
+f.close()
